9681aa9349318d0c47d39bcfb7ab80308f760f07
Maximilian H <lorganan@gmail.com>
2023-04-07 09:45:05 +0200

    everything working

--- converter.py
+++ converter.py
@@ -10,19 +10,18 @@ complete_df["EPZ"].replace({0: "no", 1: "yes"}, inplace = True)
 complete_df["Product_Categories_grp"].replace({0: "rest", 1: "accessories", 2: "footwear"}, inplace = True)
 complete_df["City_factory"] = complete_df["City_factory"].str.lower()
 complete_df["Province_State_factory"] = complete_df["Province_State_factory"].str.lower()
-# Minimum numbers
-complete_df.replace({-9: 0, 0: float("nan"), 1: 1, 2: 1000, 3: 5000}, inplace = True)
-# Middle numbers
-# complete_df.replace({-9: 0, 0: float("nan"), 1: 500, 2: 2500, 3: 7500}, inplace = True)
-# Maximum numbers
-# complete_df.replace({-9: 0, 0: float("nan"), 1: 1000, 2: 5000, 3: 10000}, inplace = True)
+for column in complete_df.columns:
+    if not "DICH" in column:
+        # Minimum numbers
+        #complete_df[column].replace({-9: 0, 0: float("nan"), 1: 1, 2: 1000, 3: 5000}, inplace = True)
+        # Middle numbers
+        complete_df[column].replace({-9: 0, 0: float("nan"), 1: 500, 2: 2500, 3: 7500}, inplace = True)
+        # Maximum numbers
+        #complete_df[column].replace({-9: 0, 0: float("nan"), 1: 1000, 2: 5000, 3: 10000}, inplace = True)
 
 df = complete_df.drop(columns = ["Parent_companies_name", "Country_parent", "Address_1_factory", "Address_2_factory", "Address_3_factory",
                        "Zip_Code_factory", "countrycode", "country_parent_num", "factory_parent_different", "ownership_grp", 
                        "TOTAL_BRANDS16", "TOTAL_BRANDS16_dich", "femployees_p_factory", "EPZ"])
-for column in df.columns:
-    if "DICH" in column:
-        df.drop(columns = column, inplace = True)
 cities_to_be_coded_list = list(set(df["City_factory"].tolist()))
 cities_to_be_coded_list = [city for city in cities_to_be_coded_list]
 
@@ -61,30 +60,48 @@ def city_encoder(city):
         row = geocode_df.loc[geocode_df["Alternate Names"].str.lower().str.contains(geocodable_entry, na = False)]
     #geocode = row["Geoname ID"].values[0]
     coordinates = row["Coordinates"].values[0]
-    return [geocodable_entry, coordinates]
-df["geocode_info"] = df["City_factory"].apply(city_encoder)
-# Split geocode_info into several columns because I don't know how to assign several new columns at once
-df["geocode_name"] = df["geocode_info"].astype(list)[0]
-coordinates = df["geocode_info"][1].str.split(", ")
-df["lat"] = coordinates[0]
-df["lon"] = coordinates[1]
+    return ", ".join([geocodable_entry, coordinates])
+df[["geocode_name", "lat", "lon"]] = df["City_factory"].apply(city_encoder).str.split(", ", expand = True)
 
 # Create dataframes for each company
-company_list = ["Adidas", "ASICS", "ASOS", "CandA", "Debenhams", "Esprit", "GAP", "HandM", "MandS", "NIKE",
+company_list = ["Adidas", "ASICS", "ASOS", "CandA", "Debenhams", "Esprit", "GAP", "G_STAR", "HandM", "MandS", "NIKE",
                 "Pentland", "Primark", "PUMA", "Under_Armour", "VF_Corp", "Amer_Sports", "Bestseller",
                 "KappAhl", "New_Balance", "Levi_Strauss", "PVH", "Tesco", "UNIQLO"]
-# Workers
-if not path.exists(path.join(dir, "workers")):
-    makedirs(path.join(dir, "workers"))
-for company in company_list:
-    for column in df.columns:
+
+# Function to export final dataframes and export as CSV
+def export_final(company_df, company, feature):
+    for column in company_df.columns:
         if company in column:
-            company_df = df[["geocode_name", column, "lat", "lon"]].rename(columns = {column: "workers"})
+            company_df = company_df[["geocode_name", column, "lat", "lon"]].rename(columns = {column: feature})
             # Including product categories
-            #company_df = df[["geocode", "Product_Categories_grp", column]].rename(columns = {column: "workers"})
+            #company_df = company_df[["geocode", "Product_Categories_grp", column]].rename(columns = {column: feature})
             if len(company_df.index) > 0:
-                company_df = company_df.loc[company_df["workers"] != 0]
-                company_df = company_df.groupby(by = ["geocode_name"], dropna = True).sum(numeric_only = True)
-                company_df = company_df.sort_values("workers", ascending = False)
-                company_df.to_csv(path.join(dir, "workers", company + ".csv"), sep = ";", encoding = "utf-8", index = False)
+                company_df = company_df.loc[company_df[feature] != 0]
+                company_df = company_df.groupby(by = ["geocode_name", "lat", "lon"], dropna = True).sum(numeric_only = True)
+                company_df = company_df.reset_index().rename(columns = {"index": "geocode_name"})
+                company_df = company_df.sort_values(feature, ascending = False)
+                company_df.to_csv(path.join(dir, feature, company + ".csv"), sep = ";", encoding = "utf-8", index = False)
+
+# Workers
+worker_df = df
+feature = "workers"
+for column in worker_df.columns:
+    if "DICH" in column:
+        worker_df = worker_df.drop(columns = column)
+if not path.exists(path.join(dir, feature)):
+    makedirs(path.join(dir, feature))
+for company in company_list:
+    export_final(worker_df, company, feature)
+
 # Has factories
+factory_df = df
+feature = "factories"
+for column in factory_df.columns:
+    for company in company_list:
+        if company in column:
+            if not "DICH" in column:
+                factory_df = factory_df.drop(columns = column)
+if not path.exists(path.join(dir, feature)):
+    makedirs(path.join(dir, feature))
+for company in company_list:
+    export_final(factory_df, company, feature)
